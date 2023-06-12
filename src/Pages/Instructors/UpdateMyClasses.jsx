014c63574864a063eb2f8fdeb099d628aef45172
toma2023 <mrinmoyehimadrita@gmail.com>
2023-06-12 11:17:16 +0600

    update myClasses edited

--- src/Pages/Instructors/UpdateMyClasses.jsx
+++ src/Pages/Instructors/UpdateMyClasses.jsx
@@ -6,7 +6,6 @@ import Swal from "sweetalert2";
 
 
 const UpdateMyClasses = () => {
-
     const { id } = useParams();
     const [addClass, , refetch] = useAddClass();
     const [updatedMyClass, setUpdatedMyClass] = useState({});
@@ -62,7 +61,7 @@ const UpdateMyClasses = () => {
                                 <input
                                     type="text"
                                     name="className"
-                                    defaultValue={selectedClass.className}
+                                    defaultValue={selectedClass?.className}
                                     placeholder="class name"
                                     className="input input-bordered w-full"
                                 />
@@ -76,7 +75,7 @@ const UpdateMyClasses = () => {
                                 <input
                                     type="text"
                                     name="price"
-                                    defaultValue={selectedClass.price}
+                                    defaultValue={selectedClass?.price}
                                     placeholder="price"
                                     className="input input-bordered w-full"
                                 />
@@ -102,58 +101,6 @@ const UpdateMyClasses = () => {
                     </div>
                 </form>
             </div>
-
-
-
-
-
-
-
-            {/* {
-            addClass.map(cls=>
-                <div key={cls._id} className="bg-[#add8e6] p-24">
-            <form onSubmit={updateMyClass}>
-                <div className="md:flex mb-8">
-                    <div className="form-control md:w-1/2 ml-4">
-                        <label className="label">
-                            <span className="label-text"> Class Name</span>
-                        </label>
-                        <label className="input-group">
-
-                            <input type="text" name="className" defaultValue={cls.className} placeholder="class name" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ml-4">
-                        <label className="label">
-                            <span className="label-text"> Price</span>
-                        </label>
-                        <label className="input-group">
-
-                            <input type="text" name="price" defaultValue={cls.price} placeholder="price" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-
-
-                    <div className="form-control md:w-1/2 ml-4">
-                        <label className="label">
-                            <span className="label-text"> Seats</span>
-                        </label>
-                        <label className="input-group">
-
-                            <input type="text" name="seats" defaultValue={cls?.seats} placeholder="seats" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                </div>
-                <div className='mt-6'>
-                    <input type="submit" value="Update Class" className="btn btn-block accent" />
-                </div>
-            </form>
-        </div>  
-                
-                )
-          }
-           */}
-
         </div>
     );
 };
